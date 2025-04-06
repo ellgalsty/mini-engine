5e38dc74639b91dda9f2eb8318085456ba67194b
ellgalsty <elengalstyann@outlook.com>
2025-04-06 16:50:47 +0400

    Cleanup

--- core/src/main/java/com/bootcamp/demo/pages/homeworks/HomeworkOne.java
+++ core/src/main/java/com/bootcamp/demo/pages/homeworks/HomeworkOne.java
@@ -1,6 +1,5 @@
 package com.bootcamp.demo.pages.homeworks;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.bootcamp.demo.engine.ColorLibrary;
 import com.bootcamp.demo.engine.Resources;
